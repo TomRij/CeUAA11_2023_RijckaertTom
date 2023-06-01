25c09ccdf33e94bb8a083340cd1acafb4bc50827
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-06-01 16:07:50 +0200

    ajout de l'examen

---
+++ CeUAA11_2023_RijckaertTom/CeUAA11_2023_RijckaertTom/Methode.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CeUAA11_2023_RijckaertTom
+{
+    struct Methode
+    {
+        /// <summary>
+        /// Créer la matrice de cryptage de la phrase phClaire grâce à la clef phClef avec la méthode de Vigenère.
+        /// </summary>
+        /// <param name="phClaire">phrase à crypter</param>
+        /// <param name="phClef">clef de cryptage</param>
+        /// <param name="MatVigenere">Matrice de cryptage de Vigenère</param>
+        public void CryptVignere(string phClaire, string phClef, out string[,] MatVigenere)
+        {
+            MatVigenere = new string[4, phClaire.Length];
+            int codeAscii;
+            int j = 0;
+            for (int i = 0; i < phClaire.Length; i++)
+            {
+                MatVigenere[1, i] = phClef[i].ToString();
+                if (j == phClef.Length)
+                {
+                    j = 0;
+                }
+                MatVigenere[1, i] = phClef[j].ToString();
+                MatVigenere[2, i] = (((int)phClef[j]) - 65).ToString();
+                if ((int)phClaire[i] + int.Parse(MatVigenere[2, i]) <= 90)
+                {
+                    codeAscii = (int)char.Parse(MatVigenere[0, i]) + int.Parse(MatVigenere[2, i]);
+                }
+                else
+                {
+                    codeAscii = (int)char.Parse(MatVigenere[0, i]) + int.Parse(MatVigenere[2, i])- 26;
+                }
+                MatVigenere[3, i] = Convert.ToChar(codeAscii).ToString();
+                j += 1;
+            }
+        }
+        /// <summary>
+        /// Créer la matrice de cryptage de phClaire avec la méthode affine.
+        /// </summary>
+        /// <param name="phClaire">phrase à crypter</param>
+        /// <param name="a">1° coefficient du cryptage</param>
+        /// <param name="b">2° coefficient du cryptage</param>
+        /// <param name="matAffine">matrice de cryptage affine</param>
+        public void CryptAffine(string phClaire, int a, int b, out string[,] matAffine)
+        {
+            int x;
+            int y;
+            matAffine = new string[4, phClaire.Length];
+            for (int i = 0; i < phClaire.Length; i++)
+            {
+                matAffine[0, i] = phClaire[i].ToString();
+                x = ((int)phClaire[i] - 65);
+                matAffine[1, i] = x.ToString();
+                y = (a * x + b) % 26;
+                matAffine[2, i] = y.ToString();
+                matAffine[3, i] = y + 65.ToString();
+            }
+        }
+        /// <summary>
+        /// Concaténer la Matrice
+        /// </summary>
+        /// <param name="Tab">Matrice à concaténer</param>
+        /// <param name="stringTab">Matrice Concaténé</param>
+        public void String_Tableau(string[,] Tab, out string stringTab)
+        {
+            // instanciation de la variable stringTab
+            stringTab = "";
+            // boucle pour se balades dans les lignes de la matrice
+            for (int ligne = 0; ligne < Tab.GetLength(0); ligne++)
+            {
+                // boucle pour se balader dans les colonnes de la matrice
+                for (int colonne = 0; colonne < Tab.GetLength(1); colonne++)
+                {
+                    // mise des données de la matrice dans la variable string stringTab
+                    stringTab += Tab[ligne, colonne] + "|";
+                }
+                // passage à la ligne
+                stringTab += "\n";
+            }
+        }
+
+        /// <summary>
+        /// Concaténer la Matrice
+        /// </summary>
+        /// <param name="Tab">Matrice à concaténer</param>
+        /// <param name="stringTab">Matrice Concaténé</param>
+        public void String_Last_Tableau(string[,] Tab, out string stringTab)
+        {
+            // instanciation de la variable stringTab
+            stringTab = "";
+            // boucle pour se balades dans les lignes de la matrice
+            for (int ligne = 3; ligne < Tab.GetLength(0); ligne++)
+            {
+                // boucle pour se balader dans les colonnes de la matrice
+                for (int colonne = 0; colonne < Tab.GetLength(1); colonne++)
+                {
+                    // mise des données de la matrice dans la variable string stringTab
+                    stringTab += Tab[ligne, colonne] + "|";
+                }
+                // passage à la ligne
+                stringTab += "\n";
+            }
+        }
+    }
+}
---
+++ CeUAA11_2023_RijckaertTom/CeUAA11_2023_RijckaertTom/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace CeUAA11_2023_RijckaertTom
+{
+    class Program
+    {
+        /// <summary>
+        /// Faire deux cryptages :
+        /// - cryptage Vigenère
+        /// - cryptage Affine
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Methode MesOutils = new Methode();
+            string choix = "";
+            Console.WriteLine("CRYPTAGE\n********");
+
+
+            while (true)
+            {
+                Console.WriteLine("Choisissez parmi les options suivantes :\n1 - Cryptage de Vigenère\n2 - Cryptage avec la méthode affine");
+                choix = Console.ReadLine();
+
+                switch (choix)
+                {
+                    // Le Cryptage de Vigenère
+                    case "1":
+                        Console.WriteLine("Entrer la phrase à crypter !");
+                        string phClaire = Console.ReadLine();
+
+                        Console.WriteLine("Entrer la clef !");
+                        string phClef = Console.ReadLine();
+                        Console.WriteLine("---------------------------");
+                        Console.WriteLine("Oups un problème est arrivé");
+                        Console.WriteLine("---------------------------");
+                        //  MesOutils.CryptVignere(phClaire, phClef, out string[,] MatVigenere);
+                        // MesOutils.String_Tableau(MatVigenere, out string stringTab);
+                        // Console.WriteLine(stringTab);
+                        break;
+
+                    // Le Cryptage de la méthode affine
+                    case "2":
+                        Console.WriteLine("Encoder la phrase à crypter !");
+                        phClaire = Console.ReadLine();
+                        Console.WriteLine("Donnez la valeur du coefficiant a (impair entre 1 et 25 sauf 13)");
+                        int a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Donnez la valeur du coefficiant b (impair entre 1 et 25 sauf 13)");
+                        int b = int.Parse(Console.ReadLine());
+                        MesOutils.CryptAffine(phClaire, a, b, out string[,] matAffine);
+                        MesOutils.String_Tableau(matAffine, out string stringTab);
+                        MesOutils.String_Last_Tableau(matAffine, out string stringTab2);
+                        Console.WriteLine("La Matrice complète !");
+                        Console.WriteLine("--------------------------------------------------------");
+                        Console.WriteLine(stringTab);
+                        Console.WriteLine("--------------------------------------------------------");
+                        Console.WriteLine("La phrase crypter !");
+                        Console.WriteLine("=========================================================");
+                        Console.WriteLine(stringTab2);
+                        Console.WriteLine("=========================================================");
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Vous avez oublié de choisier quelque chose !");
+                        break;
+                }
+                Console.WriteLine("Voulez vous recommencer (o/oui ou n/non");
+                string recom = Console.ReadLine();
+                if (recom == "n" || recom == "non")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
